3add540f7ade5c59dd52acb2ed00bda891e1a005
alias-artisoy <artisoyfernandez@gmail.com>
2022-01-17 08:32:34 +0800

    Implement local CRUD and add middleware.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import * as dotenv from "dotenv";
 import express, { application } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/item.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -16,9 +19,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/', (req, res) => {
-    return res.json('Hello World!');
-});
+app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => {
     console.log('Listening at ', PORT);
